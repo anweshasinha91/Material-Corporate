47ed9f64047d61b069c47e5d1e013ea03ae68d01
anwesha <anwesha@novatree.com>
2017-10-30 16:03:07 +0530

    Node.html.twig layed out and made responsive

--- assets/js/custom_js.js
+++ assets/js/custom_js.js
@@ -12,4 +12,8 @@ jQuery(document).ready(function () {
         jQuery('.mobile-close-drawer').css("background-color", "transparent");
         jQuery('.mobile-close-drawer').css("display", "none");
     });
+    jQuery('.node__content .col-md-4 img').parent().css('text-align','center');
+    if( jQuery('.node__content section').children().length == 0) {
+        jQuery('.node__content section').css('display','none');
+    }
 })
